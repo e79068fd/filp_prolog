df7177562080754e51aed00de8a83fa1a68e4ba8
Ilya Reshetov <ilya0reshetov@gmail.com>
2018-03-24 22:44:43 +0700

    Add bd.pl

---
+++ bd.pl
@@ -0,0 +1,4 @@
+%bus(bus_number, from, to, [distance, time, price]). % from,to - station_number
+%bus_number == 0 специальные соединения, для обозначения остановок находящихся на разных сторонах улицы
+
+%station(number, name).
