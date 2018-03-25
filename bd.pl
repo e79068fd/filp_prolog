1e170f2d2bea9d0f2c646268be695f2ff688e333
Ilya Reshetov <ilya0reshetov@gmail.com>
2018-03-25 11:39:57 +0700

    Add record in bd

--- bd.pl
+++ bd.pl
@@ -1,4 +1,14 @@
-%bus(bus_number, from, to, [distance, time, price]). % from,to - station_number
-%bus_number == 0 специальные соединения, для обозначения остановок находящихся на разных сторонах улицы
+%bus(bus_number, from, to, [distance, time, price]). % distance- км, time - минуты, from,to - station_number, price - рубли
+bus(1, a, b, [10, 25, 18]).
+bus(1, b, c, [10, 25, 18]).
+bus(1, a, c, [10, 25, 18]).
+bus(1, b, a, [10, 25, 18]).
+bus(1, c, b, [10, 25, 18]).
+bus(1, c, a, [10, 25, 18]).
 
-%station(number, name).
+bus(2, d, b, [10, 15, 20]).
+bus(2, b, c, [10, 15, 20]).
+bus(2, d, c, [10, 15, 20]).
+bus(2, c, d, [10, 15, 20]).
+bus(2, c, b, [10, 15, 20]).
+bus(2, b, d, [10, 15, 20]).
