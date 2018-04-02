7f0b80904926b3a1e923e8942dfedd23f404db34
Ilya Reshetov <ilya0reshetov@gmail.com>
2018-04-02 22:03:11 +0700

    Update station name

--- bd.pl
+++ bd.pl
@@ -1,17 +1,22 @@
-%bus(bus_number, [first, second, ..., end]). first, second, ..., end - остановки
-bus(1, [a, b, c, b, a]).
-bus(2, [d, b, c, b, d]).
-bus(3, [a, c, b, c, a]).
-bus(4, [d, c, b, c, d]).
+%bus(bus_number, [first, secon4, ..., end]). first, secon4, ..., end - остановки
+bus(1, [1, 2, 3, 2, 1]).
+bus(2, [4, 2, 3, 2, 4]).
+bus(3, [1, 3, 2, 3, 1]).
+bus(4, [4, 3, 2, 3, 4]).
 
 %station(from, to, [distance, time]) % distance - километры, time - минуты
-station(a, b, [10, 35]).
-station(b, a, [10, 35]).
-station(b, c, [10, 25]).
-station(c, b, [10, 25]).
-station(a, c, [10, 45]).
-station(c, a, [10, 45]).
-station(d, c, [10, 15]).
-station(c, d, [10, 15]).
-station(d, b, [10, 15]).
-station(b, d, [10, 15]).
+station(1, 2, [10, 35]).
+station(2, 1, [10, 35]).
+station(2, 3, [10, 25]).
+station(3, 2, [10, 25]).
+station(1, 3, [10, 45]).
+station(3, 1, [10, 45]).
+station(4, 3, [10, 15]).
+station(3, 4, [10, 15]).
+station(4, 2, [10, 15]).
+station(2, 4, [10, 15]).
+
+station_name(1, a).
+station_name(2, b).
+station_name(3, c).
+station_name(4, d).
--- main.pl
+++ main.pl
@@ -8,8 +8,8 @@
 main :-
     make_graph(BusStations),
     write(BusStations),nl,
-    Start = a,
-    End = d,
+    Start = 1,
+    End = 4,
     Weights = [0.5, 0.5, 0.5],
     find_path(BusStations, Start, Weights),
     make_answer(Start, End, Result),
