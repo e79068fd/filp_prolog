8723493c26436fd5e8f083f8f9d06c28955ecafc
Ilya <reshko1849@gmail.com>
2018-04-12 22:43:32 +0700

    Add new station_name and station

--- bd_raw.pl
+++ bd_raw.pl
@@ -2,13 +2,18 @@
 bus(2, [17, 15, 1, 2, 3, 4, 5, 6, 7, 9, 10, 9, 6, 5, 4, 3, 1, 15, 17]).
 bus(3, [17, 15, 1, 2, 3, 4, 5, 6, 7, 9, 10, 9, 6, 5, 4, 3, 1, 15, 17]).
 bus(4, [16, 1, 2, 3, 4, 5, 6, 7, 9, 10, 9, 6, 5, 4, 3, 1, 16]).
-bus(5, [16, 1, 2, 3, 4, 12, 13]).
-bus(5, [14, 18, 12, 4, 3, 1, 16]).
+%bus(5, [16, 1, 2, 3, 4, 12, 13, 19]).
+bus(5, [19, 14, 18, 12, 4, 3, 1, 16]).
 bus(12, [16, 1, 2, 3, 4, 5, 6, 7, 9, 10, 9, 6, 5, 4, 3, 1, 16]).
-bus(16, [16, 1, 2, 3, 4, 12, 13]).
-bus(16, [14, 18, 12, 4, 3, 1, 16]).
+bus(16, [16, 1, 2, 3, 4, 12, 13, 19]).
+%bus(16, [14, 18, 12, 4, 3, 1, 16]).
 %bus(19, [16, 1, 2, 3, 4, 5, 6, 7, 8, 6, 5, 4, 3, 1, 16]).
 bus(22, [17, 15, 1, 2, 3, 4, 5, 6, 7, 8, 6, 5, 4, 3, 1, 15, 17]).
+
+bus(10, []).
+bus(11, []).
+bus(24, []).
+
 %bus(23, []).
 %bus(25, []).
 %bus(26, []).
@@ -55,14 +60,13 @@ station(8, 6, [0.8, ?]).
 
 station(9, 6, [0.56, ?]).
 station(9, 10, [0.48, ?]).
-
 station(10, 9, [0.57, ?]).
 
 station(12, 4, [0.44, ?]).
 station(12, 13, [0.5, ?]).
 station(12, 5, [0.1, ?]).
 
-station(14, 18, [0.4, ?]).
+station(14, 18, [0.4, ?]). ????
 
 station(15, 1, [0.5, ?]).
 station(15, 16, [0.7, ?]).
@@ -75,6 +79,41 @@ station(17, 15, [0.8, ?]).
 
 station(18, 12, [0.5, ?]).
 
+station(13, 19, [0.5, ?]).
+station(19, 14, [0.8, ?]).
+
+station(20, 10, [1.2, ?]).
+station(20, 21, [1.2, ?]).
+station(20, 22, [1.2, ?]).
+station(20, 28, [1.2, ?]).
+
+station(21, 20, [1.2, ?]).
+station(21, 28, [1, ?]).
+
+station(22, 20, [1.2, ?]).
+station(22, 23, [0.8, ?]).
+
+station(23, 22, [0.8, ?]).
+station(23, 10, [1.1, ?]).
+station(23, 24, [1.4, ?]).
+
+station(24, 23, [1.4, ?]).
+station(24, 14, [1.2, ?]).
+station(24, 25, [1, ?]).
+
+station(25, 24, [1, ?]).
+station(25, 26, [0.9, ?]).
+
+station(26, 25, [0.9, ?]).
+station(26, 14, [1.3, ?]).
+station(26, 27, [1.4, ?]).
+
+station(27, 26, [1.4, ?]).
+station(27, 17, [1.5, ?]).
+
+station(28, 21, [1, ?]).
+station(28, 20, [1.2, ?]).
+station(28, 8, [0.5, ?]).
 
 station_name(1, cum).
 station_name(2, pl_lenina).
@@ -83,14 +122,24 @@ station_name(4, glavpochtampt).
 station_name(5, pl_novosovornaya).
 station_name(6, tgu).
 station_name(7, biblioteka_tgu).
-station_name(8, temz).
+station_name(8, temz). ??
 station_name(9, tpu).
-station_name(10, zc_octyabr).
-station_name(11, pl_dzerjinskogo).
+station_name(10, zc_octyabr). ??
 station_name(12, kraevedcheski_musei).
 station_name(13, gogol).
-station_name(14, krasnoarmeiskaya).
+station_name(14, krasnoarmeiskaya). ??
 station_name(15, ost_1905_goda).
 station_name(16, rechnoi_voksal).
-station_name(17, tgasu).
+station_name(17, tgasu). ??
 station_name(18, belinskogo).
+station_name(19, tverskaya).
+
+station_name(20, transpotnoe_kolco).
+station_name(21, lagernij_sad).
+station_name(22, tomsk1).
+station_name(23, pl_kirova).
+station_name(24, pr_komsomolsij).
+station_name(25, sibirskya_komsomolsij).
+station_name(26, sibirskya_krasnoarmeiskaya).
+station_name(27, dom_radio).
+station_name(28, uchebaya).
